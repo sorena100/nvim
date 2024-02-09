9219eb2012224dad753824ca52fadbb06304fafe
Ali NEMATOLLAHI <sorena100@gmail.com>
2024-02-09 16:31:04 +0100

    grep hovered for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,13 +1,16 @@
 return {
     {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-        local builtin = require("telescope.builtin")
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-    end
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.5',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            local builtin = require("telescope.builtin")
+            vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+            vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+            vim.keymap.set('n', '<leader>ps', builtin.grep_string, {})
+            vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+        end
     },
     {
         'nvim-telescope/telescope-ui-select.nvim',
