3f22b033c8540534a6405d874b370323ea020d5d
Ali <sorena100@gmail.com>
2024-11-28 22:25:06 +0100

    lsp and debugger for c++

--- lua/ali/plugins/dap.lua
+++ lua/ali/plugins/dap.lua
@@ -140,18 +140,89 @@ return {
 
         dap.configurations.python = configs
 
-        -- dap.adapters.codelldb = {
-        --   type = 'server',
-        --   port = "${port}",
-        --   executable = {
-        --     -- CHANGE THIS to your path!
-        --     command = '~/.local/share/nvim/mason/packages/codelldb/extension/adapter/codelldb',
-        --     args = {"--port", "${port}"},
-        --
-        --     -- On windows you may have to uncomment this:
-        --     -- detached = false,
-        --   }
-        -- }
+        dap.adapters.codelldb = {
+          type = 'server',
+          port = "${port}",
+          executable = {
+            -- CHANGE THIS to your path!
+            command = '/home/ali/.local/share/nvim/mason/packages/codelldb/extension/adapter/codelldb',
+            args = {"--port", "${port}"},
+
+            -- On windows you may have to uncomment this:
+            -- detached = false,
+          }
+        }
+
+        dap.adapters.lldb = {
+            type = 'executable',
+            command = '/usr/bin/lldb-dap-18', -- adjust as needed, must be absolute path
+            name = 'lldb'
+        }
+
+        dap.adapters.cppdbg = {
+            id = 'cppdbg',
+            type = 'executable',
+            command = '/home/ali/.vscode/extensions/ms-vscode.cpptools-1.22.11-linux-x64/debugAdapters/bin/OpenDebugAD7',
+        }
+
+        dap.configurations.cpp = {
+            {
+                name = "Launch with codelldb",
+                type = "codelldb",
+                request = "launch",
+                program = function()
+                    return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                end,
+                cwd = '${workspaceFolder}',
+                stopOnEntry = false,
+            },
+            {
+                name = 'Launch with lldb',
+                type = 'lldb',
+                request = 'launch',
+                program = function()
+                    return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                end,
+                cwd = '${workspaceFolder}',
+                stopOnEntry = false,
+                args = {},
+
+                -- 💀
+                -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+                --
+                --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+                --
+                -- Otherwise you might get the following error:
+                --
+                --    Error on launch: Failed to attach to the target process
+                --
+                -- But you should be aware of the implications:
+                -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+                -- runInTerminal = false,
+            },
+            {
+                name = "Launch with cppdbg",
+                type = "cppdbg",
+                request = "launch",
+                program = function()
+                    return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                end,
+                cwd = '${workspaceFolder}',
+                stopAtEntry = true,
+            },
+            {
+                name = 'Attach cppdbg to gdbserver :1234',
+                type = 'cppdbg',
+                request = 'launch',
+                MIMode = 'gdb',
+                miDebuggerServerAddress = 'localhost:1234',
+                miDebuggerPath = '/usr/bin/gdb',
+                cwd = '${workspaceFolder}',
+                program = function()
+                    return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                end,
+            },
+        }
         -- dap.configurations.rust = {
         --     {
         --         name = "Launch",
